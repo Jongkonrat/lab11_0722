7aae0edd914ac0c6cca0ebd4096d11b763e0e775
Jongkonrat <jongkonrat_pisit@cmu.ac.th>
2021-01-15 16:03:28 +0700

    completed

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,14 +1,14 @@
 #include<iostream>
 using namespace std;
 
-int fibonacci(int);
+long long int fibonacci(int);
 
 int main(){
     cout << fibonacci(50);
     return 0; 
 }
 
-int fibonacci(int x){
+long long int fibonacci(int x){
     if(x == 0) return 0;
     else if(x == 1) return 1;
     else if(x>1){
